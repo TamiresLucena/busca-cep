165aaba16c4c24acaf12f04a0c12fdb84108cc18
TamiresLucena <tamiresb.lucena@gmail.com>
2021-11-16 20:23:46 -0300

    refactor: swagger update

--- src/routes/addresses.js
+++ src/routes/addresses.js
@@ -4,33 +4,9 @@ const router = express.Router()
 const { addressValidation, cacheControl } = require('./middlewares')
 const { getAddress } = require('../controllers/addresses')
 
-module.exports = router.get(
-  '/',
-  addressValidation,
-  cacheControl({ maxAge: 60000 }),
-  getAddress
-
-  /*
-      #swagger.tags = ['Endereços']
-      #swagger.description = 'Endpoint para buscar endereço a partir de um CEP'
-      #swagger.parameters['zip_code'] = {
-        in: 'query',
-        description: 'Informações do produto',
-        required: true,
-        type: 'object',
-        schema: { $ref: '#/definitions/Enderecos'}
-      }
-      #swager.responses[200] = {
-          description: 'Endereço encontrado'
-      }
-      #swagger.responses[400] = {
-          description: 'CEP Inválido'
-      }
-      #swagger.responses[401] = {
-          description: 'Não autorizado'
-      }
-          #swagger.responses[404] = {
-          description: 'Não encontrado'
-      }
-    */
-)
+module.exports = router
+  .get('/address',
+    addressValidation,
+    cacheControl({ maxAge: 60000 }),
+    getAddress
+  )
